9e027e6deeb60fae1d6f457bb7bf2577f7d4dd01
Cyril leclercq <cyril.leclercq@me.com>
2024-03-13 15:04:54 +0100

    Adding setup & daemon definition

---
+++ setup.sh
@@ -0,0 +1,7 @@
+pip install -r requirements.txt
+
+cd /opt/hidscanner2mqtt/ \
+    && sudo cp systemd/hidscanner2mqtt.service /etc/systemd/system/hidscanner2mqtt.service \
+    && sudo systemctl daemon-reload \
+    && sudo systemctl restart hidscanner2mqtt.service
+    && sudo systemctl status hidscanner2mqtt.service
