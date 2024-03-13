0ec020a0440e7f72ddab579ec14a5f34ec3db2b0
Cyril leclercq <cyril.leclercq@me.com>
2024-03-13 15:04:54 +0100

    fix enable daemon service

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ pip install -r requirements.txt
 cd /opt/hidscanner2mqtt/ \
     && sudo cp systemd/hidscanner2mqtt.service /etc/systemd/system/hidscanner2mqtt.service \
     && sudo systemctl daemon-reload \
+    && sudo systemctl enable hidscanner2mqtt.service \
     && sudo systemctl restart hidscanner2mqtt.service \
     && sudo systemctl status hidscanner2mqtt.service
