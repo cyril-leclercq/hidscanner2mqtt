c3c910a55b190894f8ee8d26aa10df6195352b3f
Cyril leclercq <cyril.leclercq@me.com>
2024-03-13 15:04:53 +0100

    Code refactor to files

---
+++ configuration.py
@@ -0,0 +1,7 @@
+config = {
+    'debug': True,
+    'use_symbology_prefix' : False,
+    'mqtt_broker': 'localhost',
+    'mqtt_port': 1883,
+    'mqtt_topic': 'tag_keyboard_reader',  # to monitor, issue: mosquitto_sub -t tag_keyboard_reader
+}
